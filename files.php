f3e8a66395a6c1d57026b0dbb02336af0ded3f09
Nimo <nfediakin@mail.ru>
2022-10-14 12:45:39 +0500

    txt file read

--- files.php
+++ files.php
@@ -1,10 +1,8 @@
 <?php
 $file = fopen(__DIR__ . '/file.txt', 'r');
-for ($i = 0; $i < 4; $i = $i + 1); {
+for ($i = 0; $i < 4; $i = $i + 1) ;
+{
     echo fgetc($file);
     echo '<hr>';
-    echo '<hr>';
-
-
 }
 fclose($file);
