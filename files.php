f1ab048f81c39d01359c8fc1f7562515e77c2c97
Nimo <nfediakin@mail.ru>
2022-10-14 15:11:37 +0500

    txt file read

--- files.php
+++ files.php
@@ -1,15 +1,30 @@
 <?php
+
 //$file = fopen(__DIR__ . '/file.txt', 'r');
-//for ($i = 0; $i < 4; $i = $i + 1) ;
-//{
-//    echo fgetc($file);
-//    echo '<hr>';
+//for ($i = 0; $i < 4; $i++) {
+//    echo fgets($file);
+//    echo '<br>';
 //}
 //fclose($file);
 
-$file = fopen(__DIR__ . '/file.txt', 'r');
-while (!feof($file)) {
-    echo fgetc($file);
-    echo '<hr>';
-}
-fclose($file);
+//header('Content-type: text/html; charset=utf-8');
+//$file = fopen(__DIR__ . '/file.txt', 'r');
+//while (!feof($file)) {
+//    echo fgetc($file);
+//
+//}
+//fclose($file);
+
+//$file = fopen(__DIR__ . '/file2.txt', 'w+');
+//for ($i = 1; $i < 101; $i++) {
+//    fputs($file, $i . PHP_EOL);
+//}
+//fclose($file);
+
+//$file = fopen(__DIR__ . '/file3.txt', 'a');
+//fputs($file, 'abc' . PHP_EOL);
+//fclose($file);
+
+$data = file_get_contents(__DIR__ . '/file.txt');
+echo '<br>';
+echo $data;
